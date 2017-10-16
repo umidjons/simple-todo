72584462267070885f3ad634daab2cc6b80eee93
Umid Almatov <almatov.us@gmail.com>
2017-10-16 08:56:53 +0500

    Serving static content is configured.

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
+const path = require('path');
 const express = require('express');
 const app = express();
+const PORT = process.env.PORT || 3000;
+
+// handle app's static files
+app.use(express.static('public'));
+
+// handle vendor assets
+app.use('/vendor', express.static(path.join(__dirname, 'node_modules')));
 
 app.get('/', (req, res) => {
     res.send('hello world');
 });
 
-app.listen(3000, () => {
-    console.log('Listening on port 3000');
+app.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`);
 });
